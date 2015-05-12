1384a6809fe84abf488395929bc12f71034d6d90
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-12 11:35:48 -0400

    Initial Commit

---
+++ FindMyFriend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMyFriend
+//
+//  Created by Kunwardeep Gill on 2015-05-12.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
