051adb30e1030a90928781ae0382fe42d8fa77d3
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-12 17:38:50 -0400

    added password check code

--- NewUserViewController.m
+++ NewUserViewController.m
@@ -56,7 +56,56 @@
   // Compare password and retryPassword for equality
   // Throw up a dialog that tells them what they did wrong if they did it wrong.
   
+  NSString *username = self.usernameTextField.text;
+  NSString *password = self.passwordTextField.text;
+  NSString *passwordAgain = self.retypePasswordTextField.text;
+  NSString *errorText = @"Please ";
+  NSString *usernameBlankText = @"enter a username";
+  NSString *passwordBlankText = @"enter a password";
+  NSString *joinText = @", and ";
+  NSString *passwordMismatchText = @"enter the same password twice";
   
+  BOOL textError = NO;
+  
+  // Messaging nil will return 0, so these checks implicitly check for nil text.
+  if (username.length == 0 || password.length == 0 || passwordAgain.length == 0) {
+    textError = YES;
+    
+    // Set up the keyboard for the first field missing input:
+    if (passwordAgain.length == 0) {
+      [self.retypePasswordTextField becomeFirstResponder];
+    }
+    if (password.length == 0) {
+      [self.passwordTextField becomeFirstResponder];
+    }
+    if (username.length == 0) {
+      [self.usernameTextField becomeFirstResponder];
+    }
+    
+    if (username.length == 0) {
+      errorText = [errorText stringByAppendingString:usernameBlankText];
+    }
+    
+    if (password.length == 0 || passwordAgain.length == 0) {
+      if (username.length == 0) { // We need some joining text in the error:
+        errorText = [errorText stringByAppendingString:joinText];
+      }
+      errorText = [errorText stringByAppendingString:passwordBlankText];
+    }
+  } else if ([password compare:passwordAgain] != NSOrderedSame) {
+    // We have non-zero strings.
+    // Check for equal password strings.
+    textError = YES;
+    errorText = [errorText stringByAppendingString:passwordMismatchText];
+    [self.passwordTextField becomeFirstResponder];
+  }
+  
+  if (textError) {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:errorText message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+    [alertView show];
+    return;
+  }
+
   
 }
 
@@ -82,6 +131,5 @@
 }
 */
 
-- (IBAction)createUserButtonPressed:(UIButton *)sender {
-}
+
 @end
